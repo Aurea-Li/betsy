a53503f01d58bd20d7e45a88e6a21a95de8a0cfd
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-18 12:01:43 -0700

    Test for OrderItemsController#new

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe OrderItemsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'new' do
+    it 'succeeds' do
+      get new_order_item_path
+      must_respond_with :success
+    end
+  end
 end
