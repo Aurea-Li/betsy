6a92aa633536009bb9ecc3b6f24451e5df365982
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-18 12:17:11 -0700

    Add test template for OrderItemsController#edit

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -7,4 +7,17 @@ describe OrderItemsController do
       must_respond_with :success
     end
   end
+
+  describe 'edit' do
+    # need product and order yml fixture data before can add order_item yml fixture data
+    it 'succeeds for existing order item id' do
+      expect {
+        get edit_order_item_path(OrderItem.first)
+        must_respond_with :success
+      }
+    end
+    it 'renders 404 not found for non-existant order item id' do
+
+    end
+  end
 end
