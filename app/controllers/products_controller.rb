add2905db731d18d2e1ce9168029bf11ac898a9b
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-18 14:14:18 -0700

    Product index controller action and view created. Product index action passes test without products in db.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,46 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find_by(product_params[:id])
+  end
+
+  def update
+    @product = Product.find_by(product_params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :photo, :stock, :active, :category, :rating, :merchant_id)
+  end
+
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,9 @@ describe ProductsController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+  it "succeeds when there are no products" do
+    get products_path
+    
+    must_respond_with :success
+  end
 end
