2bfb0664b2bdf19fe09f4efdaf0a715a9318b2dd
Katricia Smith <smithkatricia@gmail.com>
2018-10-18 15:56:38 -0700

    Comment out all CRUD except for show for Merchants

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,51 +1,53 @@
 class MerchantsController < ApplicationController
-  def new
-    @merchant = Merchant.new
-  end
 
-  def create
-    #change for OAuth sign in
-    @merchant = Merchant.new(merchant_params)
+  # Sessions takes most of CRUD for Merchants?
 
-    if @merchant.save
-      flash[:success] = "Successfully created new merchant"
-      redirect_to merchant_path(@merchant)
-    else
-      flash.now[:error] = "Merchant not created. Please try again"
-      render :new, status: :bad_request
-    end
-  end
+  # def new
+  #   @merchant = Merchant.new
+  # end
+  #
+  # def create
+  #   @merchant = Merchant.new(merchant_params)
+  #
+  #   if @merchant.save
+  #     flash[:success] = "Successfully created new merchant"
+  #     redirect_to merchant_path(@merchant)
+  #   else
+  #     flash.now[:error] = "Merchant not created. Please try again"
+  #     render :new, status: :bad_request
+  #   end
+  # end
 
   def show
     @merchant = Merchant.find_by(id: params[:id])
-    render_404 unless @merchant
+    head :not_found unless @merchant
   end
 
-  def index
-    @merchants = Merchant.all
-  end
+  # def index
+  #   @merchants = Merchant.all
+  # end
 
-  def edit
-    @merchant = Merchant.find(params[:id])
-  end
+  # def edit
+  #   @merchant = Merchant.find(params[:id])
+  # end
+  #
+  # def update
+  #   @merchant = Merchant.find(params[:id])
+  #
+  #   if @merchant.save(merchant_params)
+  #     flash[:success] = "Successfully updated merchant."
+  #     redirect_to merchant_path(@merchant)
+  #   else
+  #     flash.now[:error] = "Invalid merchant information"
+  #     render :edit, status: :not_found
+  #   end
+  # end
 
-  def update
-    @merchant = Merchant.find(params[:id])
-
-    if @merchant.save(merchant_params)
-      flash[:success] = "Successfully updated merchant."
-      redirect_to merchant_path(@merchant)
-    else
-      flash.now[:error] = "Invalid merchant information"
-      render :edit, status: :not_found
-    end
-  end
-
-  def destroy
-    # Add sessions id / OAuth handling
-    @merchant.destroy
-    redirect_to merchants_path
-  end
+  # def destroy
+  #   # Add sessions id / OAuth handling
+  #   @merchant.destroy
+  #   redirect_to merchants_path
+  # end
 
 private
 
