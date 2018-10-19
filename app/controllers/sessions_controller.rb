3d71e09a0c2907f03aedeceb5c866da63d13fe0c
Katricia Smith <smithkatricia@gmail.com>
2018-10-19 14:46:29 -0700

    Change sessions controller redirect to products temporarily

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     merchant = Merchant.find_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
 
     if merchant
-      flash[:success] = "Welcome   #{merchant.username}"
+      flash[:success] = "Welcome #{merchant.username}"
     else
       merchant = Merchant.create_from_github(auth_hash)
 
@@ -13,18 +13,18 @@ class SessionsController < ApplicationController
         flash[:success] = "Logged in as merchant #{merchant.username}"
       else
         flash[:error] = "Could not create new account: #{merchant.errors.messages}"
-        redirect_to root_path
+        redirect_to products_path
         return
       end
     end
 
     session[:user_id] = merchant.id
-    redirect_to root_path
+    redirect_to products_path
   end
 
   def logout
     session[:user_id] = nil
     flash[:success] = "Successfully logged out"
-    redirect_to root_path
+    redirect_to products_path
   end
 end
