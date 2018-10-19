48d00e676c8de274a8a983a05b3542e96d19217b
Katricia Smith <smithkatricia@gmail.com>
2018-10-18 23:15:53 -0700

    Add sessions and auth route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchants
+
+  resources :sessions, only: [:new, :create]
+
+  get '/auth/:provider/callback', to: "sessions#create"
+  post '/sessions/logout', to: 'sessions#logout', as: 'logout'
 end
