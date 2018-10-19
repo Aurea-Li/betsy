8894cdec0bff83cf0f9c45b109e5a57c3ffa830e
Katricia Smith <smithkatricia@gmail.com>
2018-10-18 23:15:17 -0700

    Add create_from_github method for merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,16 @@
 class Merchant < ApplicationRecord
-
   has_many :products
+
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+
+  def self.create_from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.username = auth_hash['info']['nickname']
+    merchant.email = auth_hash['info']['email']
+    merchant.uid = auth_hash['uid']
+    merchant.provider = 'github'
+
+    return merchant
+  end
 end
