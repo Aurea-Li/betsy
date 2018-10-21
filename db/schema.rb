4e8a8ad2e7d7c0cf0413fafe64ddf86daeeaf168
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-21 10:28:34 -0700

    Add description column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_180824) do
+ActiveRecord::Schema.define(version: 2018_10_21_171911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_180824) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
