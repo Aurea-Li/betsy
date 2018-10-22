5a8290d0341511a8d9ecf0abf09bad062bff10f7
Aurea <fu.sakura@gmail.com>
2018-10-22 09:33:40 -0700

    add index method in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
 
-  
+  before_action :find_product
+
+  def index
+    @reviews = product.reviews
+  end
+
+  private
+
+  def find_product
+    @product = Product.find_all(id: params[:product_id])
+  end
 end
