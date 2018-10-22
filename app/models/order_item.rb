7c0b3e1cb63e94f6ac5c7ef7d945145dc5771e97
Aurea <fu.sakura@gmail.com>
2018-10-22 11:25:50 -0700

    add validations for order item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,11 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
+  validates :quantity, prescence: true, :numericality => { :greater_than => 0 }
+
+  validates :status,  presence: true,
+  inclusion: { in: STATUS }
+
   def price
     return self.product.price * self.quantity
   end
