5b289d56b0ad56d169e518719ef608d40e49dca1
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-22 20:54:36 -0700

    Add order items to merchant dashboard

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,4 +6,13 @@ class OrderItem < ApplicationRecord
   def price
     return self.product.price * self.quantity
   end
+
+  def self.find_order_items(merchant)
+    order_item_list = []
+    OrderItem.all.each do |order|
+      product = order.product
+      order_item_list << order unless product.merchant_id != merchant.id
+    end
+    return order_item_list
+  end
 end
