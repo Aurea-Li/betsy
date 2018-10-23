9c38a03eb221e4a21031a587cec38c1f073a0067
Aurea <fu.sakura@gmail.com>
2018-10-23 14:08:38 -0700

    add basic validation tests to product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
+  describe 'relations' do
 
-  it "must be valid" do
-    value(product).must_be :valid?
+    before do
+      @p = Product.first
+    end
+
+    it 'has a merchant' do
+
+      expect( @p.merchant ).must_be_instance_of Merchant
+
+    end
+    it 'has some reviews' do
+
+      expect( @p.reviews ).must_respond_to :each
+      expect( @p.reviews.first ).must_be_instance_of Review
+
+    end
+
+    it 'has some order items' do
+
+      expect( @p.order_items ).must_respond_to :each
+      expect( @p.order_items.first ).must_be_instance_of OrderItem
+
+    end
+  end
+
+  describe 'validations' do
+
   end
 end
