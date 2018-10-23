1190c847f56351b33a91faa91e336683a1440c7b
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-23 12:28:40 -0700

    Add new product button to dashboard. Add pending orders page.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -7,12 +7,4 @@ class OrderItem < ApplicationRecord
     return self.product.price * self.quantity
   end
 
-  def self.find_order_items(merchant)
-    order_item_list = []
-    OrderItem.all.each do |order|
-      product = order.product
-      order_item_list << order unless product.merchant_id != merchant.id
-    end
-    return order_item_list
-  end
 end
