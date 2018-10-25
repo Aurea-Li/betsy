ceb4caa9705e04ebb6c470fb9a65cb5e73ae8ab7
Aurea <fu.sakura@gmail.com>
2018-10-25 10:32:37 -0700

    add model tests for category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,38 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+  before do
+    @c = categories(:dog)
+  end
 
-  it "must be valid" do
-    value(category).must_be :valid?
+  describe 'relations' do
+    it 'has some products' do
+
+      expect( @c.products ).must_respond_to :each
+
+      @c.products.each do |product|
+        expect( product ).must_be_instance_of Product
+      end
+    end
+  end
+
+  describe 'validations' do
+    it 'is valid with a unique name' do
+
+      expect( @c ).must_be :valid?
+    end
+    it 'is invalid without a name' do
+
+      @c.name = nil
+
+      expect( @c ).wont_be :valid?
+    end
+
+    it 'is invalid with a duplicate name' do
+
+      dup_category = Category.new(name: @c.name )
+
+      expect( dup_category ).wont_be :valid?
+    end
   end
 end
